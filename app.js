27632cc4f1fb97fe506e69983d0d5de056902608
rhare86 <rhare86@gmail.com>
2022-05-24 23:02:50 -0500

    I'm doing this again

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
 
-// const generatePage = require('./src/page-template');
-
-
-inquirer 
+inquirer
   .prompt([
     {
       type: 'input',
@@ -16,17 +12,16 @@ inquirer
   .then(answers => console.log(answers));
 
 
-
 // const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-//    if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
-
 
 
 
